ed5aa8f60c8c3d371a8e270f7e8f2d6b018ea0e7
linhng98 <linh1612340@gmail.com>
2024-01-21 10:25:25 +0700

    fix: remove ec2 frp instance, use router portforward and ddns instead

--- external/iac/homelab/ec2/frp-server/eip.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_eip" "this" {
-  network_interface = aws_instance.this[0].primary_network_interface_id
-  instance          = aws_instance.this[0].id
-}
--- external/iac/homelab/ec2/frp-server/eip_outputs.tf
+++
@@ -1,4 +0,0 @@
-output "eip_ip" {
-  description = "The ip of eip"
-  value       = aws_eip.this.public_ip
-}
